2e653d7d47935ba627058504a2f208dc9a3d818d
long <932841304@qq.com>
2023-04-17 17:09:00 +0800

    4/17

--- myrouter-api/src/main/java/com/example/myrouter_api/thread/BackGroundPoster.java
+++ myrouter-api/src/main/java/com/example/myrouter_api/thread/BackGroundPoster.java
@@ -1,4 +1,54 @@
 package com.example.myrouter_api.thread;
 
-public class BackGroundPoster {
+import com.example.myrouter_api.action.RouterAction;
+import com.example.myrouter_api.core.MyRouter;
+import com.example.myrouter_api.extra.ActionWrapper;
+import com.example.myrouter_api.extra.Consts;
+import com.example.myrouter_api.result.RouterResult;
+
+public class BackGroundPoster implements Runnable, Poster{
+
+    private final ActionPostQueue queue;
+    private volatile boolean excuteRunning;
+
+    public BackGroundPoster() {queue = new ActionPostQueue();}
+
+    @Override
+    public void enqueue(ActionPost actionPost) {
+        synchronized (this) {
+            queue.enqueue(actionPost);
+            if (!excuteRunning) {
+                excuteRunning = true;
+                PosterSupport.executorService().execute(this);
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+             try {
+                while (true) {
+                    ActionPost actionPost = queue.poll(1000);
+                    if (actionPost == null) {
+                        synchronized (this) {
+                            if (actionPost == null) {
+                                excuteRunning = false;
+                                return;
+                            }
+                        }
+                    }
+                    ActionWrapper actionWrapper = actionPost.actionWrapper;
+                    RouterAction routerAction = actionWrapper.getRouterAction();
+                    RouterResult result = routerAction.invoke(actionPost.context, actionPost.params);
+                    actionPost.actionCallBack.onResult(result);
+                    actionPost.releasePendingPost();
+                }
+             } catch (InterruptedException e) {
+                 MyRouter.logger.e(Consts.TAG, Thread.currentThread().getName() + "was interrupted");
+             }
+        } finally {
+            excuteRunning = false;
+        }
+    }
 }
--- myrouter-api/src/main/java/com/example/myrouter_api/thread/HandlerPoster.java
+++ myrouter-api/src/main/java/com/example/myrouter_api/thread/HandlerPoster.java
@@ -1,4 +1,73 @@
 package com.example.myrouter_api.thread;
 
-public class HandlerPoster {
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.os.SystemClock;
+
+import androidx.annotation.NonNull;
+
+import com.example.myrouter_api.action.RouterAction;
+import com.example.myrouter_api.extra.ActionWrapper;
+import com.example.myrouter_api.result.RouterResult;
+
+/*
+处理主线程切换
+ */
+public class HandlerPoster extends Handler implements Poster {
+
+    private int maxMillisInsideHandleMessage;
+    private final ActionPostQueue queue;
+    private boolean handleActive;
+
+    protected HandlerPoster(Looper looper, int maxMillisInsideHandleMessage) {
+        super(looper);
+        this.maxMillisInsideHandleMessage = maxMillisInsideHandleMessage;
+        queue = new ActionPostQueue();
+    }
+
+    @Override
+    public void handleMessage(@NonNull Message msg) {
+        boolean rescheduled = false;
+        try {
+            long started = SystemClock.uptimeMillis();
+            while (true) {
+                ActionPost actionPost = queue.poll();
+                if (actionPost == null) {
+                    synchronized (this) {
+                        if (actionPost == null) {
+                            rescheduled = false;
+                            return;
+                        }
+                    }
+                }
+
+                ActionWrapper actionWrapper = actionPost.actionWrapper;
+                RouterAction routerAction = actionWrapper.getRouterAction();
+                RouterResult result = routerAction.invoke(actionPost.context, actionPost.params);
+                actionPost.actionCallBack.onResult(result);
+                actionPost.releasePendingPost();
+
+                long timeInMethod = SystemClock.uptimeMillis() - started;
+                if (timeInMethod >= maxMillisInsideHandleMessage) {
+                    if (!sendMessage(obtainMessage()))
+                        throw new RuntimeException("could not send handler message");
+                    rescheduled = true;
+                    return;
+                }
+            }
+        } finally {
+            handleActive = rescheduled;
+        }
+    }
+
+    @Override
+    public void enqueue(ActionPost actionPost) {
+        queue.enqueue(actionPost);
+        if (!handleActive) {
+            handleActive = true;
+            if (!sendMessage(obtainMessage()))
+                throw new RuntimeException("could not send handler message");
+        }
+    }
 }
